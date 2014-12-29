f3db4e3bf3e15527b78d363c5e27f4bd93b19f9a
Mohammad Rafiee <rafiee@itrc.ac.ir>
2014-12-29 11:34:14 +0330

    script initials

---
+++ scrum-daily.py
@@ -0,0 +1,15 @@
+#!/bin/python
+members = ('a','b','c','d','e','f','g')
+
+for i in members:
+	raw_input('Che khabar :) ')
+	if PO.feedback == true:
+		print "Khaste nabashin! :)"
+	else:
+		print "Saritar jamesh konin!"
+		print "Khaste nabashin :("
+	if SADEGH.comment:
+		print SADEGH.comment
+	
+	print "Dev ro ham update konin!"
+	continue
