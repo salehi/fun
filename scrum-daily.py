428b732fb77dc0296dd361864bf275ba15ad3ef7
mohammad <m.rafieee@gmail.com>
2015-01-18 12:07:06 +0330

    minor changes

--- scrum-daily.py
+++ scrum-daily.py
@@ -1,35 +1,36 @@
 #!/bin/python
 import os
-members = ('a','b','c','d','e','f','g')
+
+members = ('a', 'b', 'c', 'd', 'e', 'f', 'g')
 
 print "Thanks FUN Writer! You help the team to get in the better feeling"
 print "Thanks for this great git repository"
 print "man az haminja to jam az shoma tashakkor mikonam ;)"
 
-   
+
 class Meetings:
     def init(self):
-        
+
         return True
 
     def scrum_daily_meeting(self):
         # TODO: to complete and merge specified codes
         for i in members:
-                raw_input('Che khabar :) ')
-                if PO.feedback == True:
-                        print "Khaste nabashin! :)"
-                else:
-                        print "Saritar jamesh konin!"
-                        print "Khaste nabashin :("
-                if SADEGH.comment:
-                        ignore( SADEGH.comment )
-                else
-                        PO.mood("nafasi rahat")
-                print "Dev ro ham update konin!"
-                continue
-    
+            raw_input('Che khabar :) ')
+            if PO.feedback == True:
+                print "Khaste nabashin! :)"
+            else:
+                print "Saritar jamesh konin!"
+                print "Khaste nabashin :("
+            if SADEGH.comment:
+                ignore(SADEGH.comment)
+            else
+                PO.mood("nafasi rahat")
+            print "Dev ro ham update konin!"
+            continue
+
         return True
-    
+
     def delivery_meeting(self):
         # TODO: to complete and merge specified codes
         if jalase_tahvil:
@@ -40,7 +41,8 @@ class Meetings:
                     PO.mood("Get Rough and turn off")
                     PO.tell("Stop it")
                     PO.think("ajab kari kard! to 4 rooz kare 7 maahe 5 nafaro bast!! zereshk!!!!")
-                    members["r"].tell("foote, Hoooooh, hahahaha :)))") and members["r"].think("in bache oomade be man mige! mikhad be man yad bede!! bishin binim baghali")
+                    members["r"].tell("foote, Hoooooh, hahahaha :)))") and members["r"].think(
+                        "in bache oomade be man mige! mikhad be man yad bede!! bishin binim baghali")
                     sadegh.think("What is the matter!")
                     sadegh.mood(":(")
                     sadegh.learn("Get off") and sadegh.build("self")
@@ -48,110 +50,118 @@ class Meetings:
                 if related_to_r(t):
                     print "depende ardalane"
         return "depende ardalane"
-    
+
     def grooming_meeting(self):
         # TODO: to complete and merge specified codes
         if new_sprint:
-            add_gbp_task_at_first(asignee=("f","a","j","r")
+            add_gbp_task_at_first(asignee=("f", "a", "j", "r")
             print "inam az in"
         return "ma kheili khoob pish mirim"
 
 
 def end_of_phase(self):
-        # TODO: to complete and merge specified codes
-        everyone.hurryup = True
-        while ! documents
+    # TODO: to complete and merge specified codes
+    everyone.hurryup = True
+    while not documents:
+        everyone.write(documents)
+        print("halla chi misheeeeee , age naresim chi misheee")
+        PO.think("khob maloome chizi nemishe ina cheghad khengan")
+        members.worry = True
+    return True
 
-        return True
 
 def birthday(self):
-        # TODO: to complete and merge specified codes
-        return True
+    # TODO: to complete and merge specified codes
+    return True
 
-def pishraft(self,member):
-	if member == SADEGH:
-		return True
-	elif member.has("CAKE") == True
-		return True
-	else
-		return False
+
+def pishraft(self, member):
+    if member == SADEGH:
+        return True
+    elif member.has("CAKE") == True:
+        return True
+    else:
+        return False
+
+
 def release(self):
-	if ! os.path.exist(*.iso)
-        	print "iso besazim"
-        	os.run("bash touch XaaS.iso")
-        	print "inam az IaaS, iiiinnnnneeeeeeeeeehhhhhh"
+    if not os.path.exist("*.iso"):
+        print "iso besazim"
+    os.run("bash touch XaaS.iso")
+    print "inam az IaaS, iiiinnnnneeeeeeeeeehhhhhh"
 
 
 ################################################################
 # TODO: To be merged the code below in functions above
 
-if pish_berim == True and i > (2*sprint_days.size()/3)
-	print "demo nadarim"
-	print "hoghoogh hatoono gereftin?"
-	
-else if demo_darim == True:
-	print "chand rooz bishtar namoonde, hamegi hamkari konin ke khoob pish berim"
-	pish_berim = True;
-	
-else if AkhareFaaz:
-		print "demo darim"
-		demo_darim=True
+if pish_berim == True and i > (2 * sprint_days.size() / 3):
+    print "demo nadarim"
+    print "hoghoogh hatoono gereftin?"
 
-if foolad : 
-	PO.mood("jalase")
-	TEAM.mood("baghali shodim")
-	foolad.think("aamo koojan ina?! nimfahman man injam?!")
-	foolad.mood(NULL)
-	foolad.create("Job for others")
-	foolad.sleep("1W")
-	Others.build("foolad jobs", time="1W")
-#	Nothing will be printed!! Since foolad is exist! :))) 
-#	Do NOT any Idea will be "print"ed!!
-#	Do NOT any Work will be "print"ed!!
-#	Do NOT any Work will be "print"ed!!
-#	Just silence will be "print"ed!!
-	print NULL
-	
-	if foolad.see(sadegh):
-		foolad.think("in bache inja chikar mikone")
-		sadegh.think("dare be my age FEKR mikone!!")
-		sadegh.tell("salam^, chetoriN^?")
-		foolad.mood("!")
-		foolad.tell("salam, khoobam, shooma chitowri?!")
-		sadegh.tell("bad nistam^^")
-	
-if seyed.see(sadegh) and exist(i) for i in members
-	seyed.tell("agha ma bache boodim chikar mikardim?")
-	sadegh.think("ey baw! ajab giri oftadima! har ki az har jayee be my age kaar dare!")
-	sadegh.think("nakone ina enghade senneshoon rafte bala ke dige cell e khakestari nadaran!?!")
-	sadegh.tell(NULL)
-	seyed.tell("boro beshin darseto bekhoon bache! ma ham senne to boodim azin kara nemikardim ke!")
-	i.mood(":))))")
-	sadegh.tell(NULL)
-	sadegh.mood(":(")
-	sadegh.tell("man az in kare shoma narahat misham").private_to(seyed)
+elif demo_darim == True:
+    print "chand rooz bishtar namoonde, hamegi hamkari konin ke khoob pish berim"
+    pish_berim = True;
+
+elif AkhareFaaz:
+    print "demo darim"
+    demo_darim = True
+
+if foolad:
+    PO.mood("jalase")
+    TEAM.mood("baghali shodim")
+    foolad.think("aamo koojan ina?! nimfahman man injam?!")
+    foolad.mood(NULL)
+    foolad.create("Job for others")
+    foolad.sleep("1W")
+    Others.build("foolad jobs", time="1W")
+    # Nothing will be printed!! Since foolad is exist! :)))
+    # Do NOT any Idea will be "print"ed!!
+    #	Do NOT any Work will be "print"ed!!
+    #	Do NOT any Work will be "print"ed!!
+    #	Just silence will be "print"ed!!
+    print NULL
+
+    if foolad.see(sadegh):
+        foolad.think("in bache inja chikar mikone")
+        sadegh.think("dare be my age FEKR mikone!!")
+        sadegh.tell("salam^, chetoriN^?")
+        foolad.mood("!")
+        foolad.tell("salam, khoobam, shooma chitowri?!")
+        sadegh.tell("bad nistam^^")
+
+if seyed.see(sadegh) and exist(i):
+    for i in members:
+        seyed.tell("agha ma bache boodim chikar mikardim?")
+        sadegh.think("ey baw! ajab giri oftadima! har ki az har jayee be my age kaar dare!")
+        sadegh.think("nakone ina enghade senneshoon rafte bala ke dige cell e khakestari nadaran!?!")
+        sadegh.tell(NULL)
+        seyed.tell("boro beshin darseto bekhoon bache! ma ham senne to boodim azin kara nemikardim ke!")
+        i.mood(":))))")
+        sadegh.tell(NULL)
+        sadegh.mood(":(")
+        sadegh.tell("man az in kare shoma narahat misham").private_to(seyed)
 
 if members["fun writer"].mood("pishraft"):
-	if pishraft.Analysis(Sadegh):
-		members["fun writer"].pattern = pishraft.result
-	else:
-		BroadCast("Sadegh nardebooni pishraft mikone, shibesh amoodie!")
-		members["fun writer"].mood = ":))))))))"
-		Sadegh.mood = "!"
-		Sadegh.think = "What is the matter!!! nardeboon?!!!"
+    if pishraft.Analysis(Sadegh):
+        members["fun writer"].pattern = pishraft.result
+    else:
+        BroadCast("Sadegh nardebooni pishraft mikone, shibesh amoodie!")
+        members["fun writer"].mood = ":))))))))"
+        Sadegh.mood = "!"
+        Sadegh.think = "What is the matter!!! nardeboon?!!!"
 
-if Ehsan.comment == True or Ehsan.comment == False :
-	print Ehsan.comment
-	PO.tell("khob chetori mishe inkaro kard?")
-	print "intori :-{"
+if Ehsan.comment == True or Ehsan.comment == False:
+    print Ehsan.comment
+    PO.tell("khob chetori mishe inkaro kard?")
+    print "intori :-{"
 
-if Ardalan.have_problem == True :
-	Sadegh.ehzar()
-	sleep 1h
-	ignore(Sadegh)
+if Ardalan.have_problem == True:
+    Sadegh.ehzar()
+    os.system("sleep 3600")
+    ignore(Sadegh)
 
 
-	
+
 
 
 # exit Failure anyway!
